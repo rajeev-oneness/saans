d91c9cde65e01ccc5d984c42df9a049a6f789948
HalderPapiya <papiya@onenesstechs.in>
2021-10-04 18:46:59 +0530

    change

--- resources/views/admin/sub-category/index.blade.php
+++ resources/views/admin/sub-category/index.blade.php
@@ -32,8 +32,8 @@
             </a>
             <!-- <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
                 <i class="fas fa-minus"></i></button> -->
-            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-                <i class="fas fa-times"></i></button>
+            <!-- <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                <i class="fas fa-times"></i></button> -->
         </div>
       </div>
       <div class="card-body">
